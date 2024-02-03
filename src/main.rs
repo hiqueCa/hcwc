6e919d6df88c044d23434607b1105bd885eb6e10
Henrique Pousa Caltram <henrivai@hotmail.com>
2024-02-03 13:08:56 -0300

    Implement raw command option flow control behaviour

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,15 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    let command_option: &String = &args[1];
     let file_name: &String = &args[2];
 
     let file_content: String = fs::read_to_string(&file_name).expect(
         "Could not ready specified file content!"
     );
-    dbg!(file_content);
+
+    if command_option == "-c" {
+        let file_bytes: &usize = &file_content.len();
+        println!("{file_bytes}");
+    }
 }

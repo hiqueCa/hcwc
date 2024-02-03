7e1f0008b563311bb9e29ca234b40e9cab1a88c1
Henrique Pousa Caltram <henrivai@hotmail.com>
2024-02-03 11:47:09 -0300

    Implement file reading behaviour

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    dbg!(args);
+
+    let file_name: &String = &args[2];
+
+    let file_content: String = fs::read_to_string(&file_name).expect(
+        "Could not ready specified file content!"
+    );
+    dbg!(file_content);
 }
